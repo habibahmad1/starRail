7e5d0e3756eb26070f4e89f0424ae18842abec98
habibahmad1 <oregairu2597@gmail.com>
2024-04-14 10:16:08 +0700

    button work

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const gacha1 = document.querySelector(".button-right .gacha1");
+const gacha10 = document.querySelector(".button-right .gacha10");
+const videoR = document.querySelector(".video-r");
+
+gacha1.addEventListener("click", () => {
+  videoR.style.display = "block";
+  videoR.play();
+});
