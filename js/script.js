5c1b1c04d82093dd9d1b5394ef57b2728067772a
habibahmad1 <oregairu2597@gmail.com>
2024-04-15 18:10:10 +0700

    edit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 const gacha1 = document.querySelector(".button-right .gacha1");
 const gacha10 = document.querySelector(".button-right .gacha10");
-const videoR = document.querySelector(".video-r");
+const videoR = document.querySelector(".video-ssr");
 const isMobile = window.matchMedia("(max-width: 600px)").matches;
+// Item Gacha
+let items = [
+  document.querySelector(".item1"),
+  document.querySelector(".item2"),
+  document.querySelector(".item3"),
+  document.querySelector(".item4"),
+  document.querySelector(".item5"),
+  document.querySelector(".item6"),
+  document.querySelector(".item7"),
+];
+
+const sound = document.querySelector(".sound");
 
 gacha1.addEventListener("click", () => {
+  // Sistem gacha
+  const pity = 0;
+  const pityCount = 0;
+
+  const randomIndex = Math.floor(Math.random() * items.length);
+  const randomItem = items[randomIndex];
+
   videoR.style.display = "block";
   videoR.removeAttribute("controls");
   videoR.play();
@@ -22,5 +41,8 @@ gacha1.addEventListener("click", () => {
 
   videoR.addEventListener("ended", () => {
     videoR.style.display = "none";
+    randomItem.style.display = "block";
+    sound.play();
+    console.log(randomItem);
   });
 });
